2ddc25b85bce94f8667854f2f936cb2ec8056c29
mary-ue <maryandreeva100@gmail.com>
2023-11-05 01:21:34 +0300

    primitives

---
+++ src/01-primitives.ts
@@ -0,0 +1,31 @@
+// number
+let x = 10;
+// let y = 1234n;  // должны быть целыми, не используем дробную часть ×1234.5n
+let z = NaN;
+
+// string
+let str1: string;
+str1 = 'Hello';
+
+let symb = Symbol('as'); // let symb: symbol
+
+// boolean
+let o = true;
+o = false;
+
+// nothing
+let h: undefined = undefined;
+let g: null = null;
+
+// literal (при использовании const)
+const num = 108; // const num: 108
+const str2 = 'str'; // const str2: "str"
+
+// universal 
+let mm: any = 1;
+mm = 'str';
+mm = [];
+
+let xx: unknown = 2; // значение переменной узнаем позднее
+// прежде, чем использовать методы, нужно будет уточнить тип
+if (typeof xx === 'string') xx.toUpperCase();
